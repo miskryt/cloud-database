6e919eac4a8dff6cb9b50441e79b13f85308a5cf
Mikhail Iskrytski <miskrytsi@solvd.com>
2022-08-26 12:41:44 +0300

    auth process

---
+++ frontend/src/app/pagenotfound/page-notfound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound',
+  templateUrl: './page-notfound.component.html',
+  styleUrls: ['./page-notfound.component.css']
+})
+export class PageNotfoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
